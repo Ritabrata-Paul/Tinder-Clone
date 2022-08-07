1a35f33b50c311411b11c6e0925bde11fd6b2240
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-08-07 20:56:27 +0530

    2st

---
+++ src/Component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Hey beautifull
+
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/Component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      I am a good person
+    </div>
+  )
+}
+
+export default Header;
